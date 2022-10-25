9135daf7efc4a9d68e27771d3068a9cb08b2cae7
romulogoncalves94 <romulogoncalves94@hotmail.com>
2022-10-25 20:33:45 -0300

    feat: tratamento de exceção para objeto não encontrado

--- src/main/java/com/romulo/helpdesk/services/TecnicoService.java
+++ src/main/java/com/romulo/helpdesk/services/TecnicoService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.romulo.helpdesk.domain.Tecnico;
 import com.romulo.helpdesk.repositories.TecnicoRepository;
+import com.romulo.helpdesk.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class TecnicoService {
@@ -16,6 +17,6 @@ public class TecnicoService {
 	
 	public Tecnico findById(Integer id) {
 		Optional<Tecnico> obj = repository.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id: " + id));
 	}
 }
