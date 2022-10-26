d2c1fccc828ca5c046c0a9a1c3905c2e29d8881d
romulogoncalves94 <romulogoncalves94@hotmail.com>
2022-10-25 21:43:03 -0300

    feat: create Tecnico

--- src/main/java/com/romulo/helpdesk/domain/Tecnico.java
+++ src/main/java/com/romulo/helpdesk/domain/Tecnico.java
@@ -2,12 +2,14 @@ package com.romulo.helpdesk.domain;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import javax.persistence.Entity;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.romulo.helpdesk.domain.dtos.TecnicoDTO;
 import com.romulo.helpdesk.domain.enums.Perfil;
 
 @Entity
@@ -29,6 +31,17 @@ public class Tecnico extends Pessoa{
 		super(id, nome, cpf, email, senha);
 		addPerfil(Perfil.CLIENTE);
 	}
+	
+	public Tecnico(TecnicoDTO obj) {
+		super();
+		this.id = obj.getId();
+		this.nome = obj.getNome();
+		this.cpf = obj.getCpf();
+		this.email = obj.getEmail();
+		this.senha = obj.getSenha();
+		this.perfis = obj.getPerfis().stream().map(x -> x.getCodigo()).collect(Collectors.toSet());
+		this.dataCriacao = obj.getDataCriacao();
+	}
 
 	public List<Chamado> getChamados() {
 		return chamados;
@@ -37,5 +50,4 @@ public class Tecnico extends Pessoa{
 	public void setChamados(List<Chamado> chamados) {
 		this.chamados = chamados;
 	}
-	
 }
